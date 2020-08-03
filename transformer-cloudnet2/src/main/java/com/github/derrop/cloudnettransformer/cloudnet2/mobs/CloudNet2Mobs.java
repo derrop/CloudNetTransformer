0c9f8cfa9099c7820c5d3bbac98be17965ce2197
Robin <derrop@outlook.de>
2020-08-03 02:19:02 +0200

    Add default profile for CloudNet 2 NPCs

--- transformer-cloudnet2/src/main/java/com/github/derrop/cloudnettransformer/cloudnet2/mobs/CloudNet2Mobs.java
+++ transformer-cloudnet2/src/main/java/com/github/derrop/cloudnettransformer/cloudnet2/mobs/CloudNet2Mobs.java
@@ -4,6 +4,7 @@ import com.github.derrop.cloudnettransformer.cloud.deserialized.CloudSystem;
 import com.github.derrop.cloudnettransformer.cloud.deserialized.database.Database;
 import com.github.derrop.cloudnettransformer.cloud.deserialized.npcs.placed.NPCAction;
 import com.github.derrop.cloudnettransformer.cloud.deserialized.npcs.placed.PlacedNPC;
+import com.github.derrop.cloudnettransformer.cloud.deserialized.npcs.placed.ProfileProperty;
 import com.github.derrop.cloudnettransformer.cloud.executor.CloudReaderWriter;
 import com.github.derrop.cloudnettransformer.cloud.executor.annotation.DescribedCloudExecutor;
 import com.github.derrop.documents.Document;
@@ -75,7 +76,11 @@ public class CloudNet2Mobs implements CloudReaderWriter {
                     new UUID(mob.get("uniqueId", UUID.class).getMostSignificantBits(), 0),
                     mob.getString("display"),
                     mob.getString("displayMessage"),
-                    Collections.emptyList(), // TODO add default skin
+                    Collections.singletonList(new ProfileProperty(
+                            "textures",
+                            "ewogICJ0aW1lc3RhbXAiIDogMTU5NjQwOTAyNDU3NiwKICAicHJvZmlsZUlkIiA6ICIyNjRmMGVlMzY0NmU0NDc1OWE4NmU5ZjY3NjE2N2Y1NyIsCiAgInByb2ZpbGVOYW1lIiA6ICJKcmFtZWUiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYmM2ZjgxYWYzMWUwZGNkNGZlZTRiYmIwNzk3ZGUwYzBhOTU0OTlhZjJlMDNhMzY0MjQ4YTQwNTIwMTdiM2U2ZCIKICAgIH0KICB9Cn0=",
+                            "B05ubm5ysH4EyHhIMlrD2esNbgVDX6tDww5pFLBNQI/cs6jOIhuAUBskIQEXyv1aeoWEPNhAZjcZCOj/9wZBRhqDy0Ulj++DFQTj7LkO2lR5E+BvydalRFwnMQvcpuySGvb9fn1I1oaVDJyxWtpBKQ1cQ5iVewpvLrd0pTu+RyFrhnoTu18BN1hx/xDgpAt3iFauY7uwiD1m0KRii1CB415X9Bil5ivv/7KhxAVDlCPpF1oTQ6iCf8uEw1QFw+R+Vv/1wb/EPWGwxWEAnEm5kv9NhzdOaz+WXMxpiAwPGvFum5uRYtL3byleUcqKTSFbsnFHPc/Yb4lNeaX+kHPeUzwYBZ2EtGZxtO8ewoPtEWGAt585ZUsLG4QfhTSXt+2SLYOVGsZhTi59P8aWXxmQHVxmNLLKgB7QQ5uvMTPYYZy3zlllKQYefT1KQMtKTmRDhjqYEe9Mm1a7Le0K33ArtWpcX8Pgqda7Jf2mbJo9tT0Q3BF+P5fqf1p1fB4VO3dS3oDA9fHih3TzbY1+M/0ikwfVjJXsIq/URlhw+ayZPUE52duJuX9PyT/tjT1WdaLzsMKVWF3K8/9mXviMjA2qSLdWiJXybpceksfd3AD7a+YBuakl4r0t2KraGKtRXfWp7GCJ5zzTZovLrRe3n28P4/5SXHhphZShmurP1FpCUIw="
+                    )),
                     mob.getString("itemName"),
                     mob.getString("targetGroup"),
                     position.getString("group"),
